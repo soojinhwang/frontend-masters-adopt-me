eba62f84e304c5449015c69d84f960856409e091
Soojin Hwang <soojiny.hwang@gmail.com>
2023-11-06 16:27:19 +0000

    Hook used to vary search input date

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            onChange={(event) => setLocation(event.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
