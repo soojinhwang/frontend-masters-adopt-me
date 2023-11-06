0a82e24ddf156e64f4b93407444fdfa4254abcf5
Soojin Hwang <soojiny.hwang@gmail.com>
2023-11-06 16:16:55 +0000

    App.jsx fixed to return SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <SearchParams />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
