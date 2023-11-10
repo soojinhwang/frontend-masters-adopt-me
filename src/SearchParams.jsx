63c86bfc15cedcbcadda2ec6b4a23b785e81d53e
Soojin Hwang <soojiny.hwang@gmail.com>
2023-11-10 14:23:08 +0000

    Added breed info

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,6 +4,9 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
@@ -21,13 +24,28 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={(e) => {
-              setAnimal(e.target.value);
+            onChange={(event) => {
+              setAnimal(event.target.value);
             }}
           >
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => {
+              setBreed(event.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
