e5cd88c2013ff8029669db38bf4e998afdb3c0c8
Soojin Hwang <soojiny.hwang@gmail.com>
2023-12-11 14:43:10 +0000

    Added API functions to fetch pets

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  useEffect(() => {});
+  requestPets();
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
