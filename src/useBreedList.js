59d2a2834b8fb47606cc369f9c21aa87fe1ef2a2
Soojin Hwang <soojiny.hwang@gmail.com>
2023-12-11 15:36:27 +0000

    Creating custom hook useBreedList

---
+++ src/useBreedList.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    }
+  });
+}
