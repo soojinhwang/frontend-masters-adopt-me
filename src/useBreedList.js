542f7911df8927e25813749daa245a9efa5fb0c0
Soojin Hwang <soojiny.hwang@gmail.com>
2023-12-11 15:46:12 +0000

    useBreedList effect function completed

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -9,6 +9,25 @@ export default function useBreedList(animal) {
   useEffect(() => {
     if (!animal) {
       setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
     }
-  });
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
 }
