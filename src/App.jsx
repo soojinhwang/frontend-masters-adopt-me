3b5f45b7596a12e6f2f46a62cc8d6d68d00f7a15
Soojin Hwang <soojiny.hwang@gmail.com>
2024-01-25 17:25:52 +0000

    Used React Query to optimise application performance by caching pet details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryCleintProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
