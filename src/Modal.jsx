ceaf7129805191df20648e2a55312c2f987a6c92
Soojin Hwang <soojiny.hwang@gmail.com>
2024-02-09 16:44:33 +0000

    Implemented modal to be rendered on pet details page

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) =>  {
+const Modal = ({ children }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
-    elRef.current = document.createElement('div');
+    elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
@@ -12,9 +12,9 @@ const Modal = ({ children }) =>  {
     modalRoot.appendChild(elRef.current);
 
     return () => modalRoot.removeChild(elRef.current);
-  }, [];)
+  }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
-}
+};
 
 export default Modal;
